ca7e9b632b5410885151bd231da6d6c286ce5a05
beodwilson <beodwilson@gmail.com>
2022-10-18 01:02:49 +0000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Test from "./test";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="bg-blue-500 w-24 h-24 rounded-lg">This is a test</div>
+        <Test />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Test() {
+  var a: number = 3;
+
+  return <div className=" text-blue-400">{`${a}`}</div>;
+}
+
+export default Test;
